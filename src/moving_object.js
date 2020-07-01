014edb344e53b5d58a741a7ecce908a3bf46fd18
Kevin Proudler <kevinproudler@gmail.com>
2020-07-01 08:54:12 -0400

    move+draw

--- src/moving_object.js
+++ src/moving_object.js
@@ -6,10 +6,15 @@ function MovingObject(options){
 }
 
 MovingObject.prototype.draw = function draw(ctx) {
-    
+    ctx.fillStyle = this.color;
+
     ctx.beginPath();
     ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);
     ctx.fill();
+}
+
+MovingObject.prototype.move = function move() {
+    this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]];
 }
 
 module.exports = MovingObject;
