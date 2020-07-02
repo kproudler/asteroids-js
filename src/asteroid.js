7cce5703d883e6f18b199735ad37be4cf0cf5fb5
Kevin Proudler <kevinproudler@gmail.com>
2020-07-02 07:02:46 -0400

    finished project

--- src/asteroid.js
+++ src/asteroid.js
@@ -1,33 +1,38 @@
-const MovingObject = require('./moving_object.js');
-const Util = require('./util.js');
+const Util = require("./util");
+const MovingObject = require("./moving_object");
+const Ship = require("./ship");
+const Bullet = require("./bullet");
 
-const Defaults = {
-    color: "#ededed",
-    radius: 25
-}
-
+const DEFAULTS = {
+  COLOR: "#505050",
+  RADIUS: 25,
+  SPEED: 4
+};
 
 function Asteroid(options) {
-    options.color = Defaults.color;
-    options.radius = Defaults.radius;
-    options.pos = options.pos;
-    options.vel = options.vel;
-    
-    MovingObject.call(this, options);
+  options = options || {};
+  options.color = DEFAULTS.COLOR;
+  options.pos = options.pos || options.game.randomPosition();
+  options.radius = DEFAULTS.RADIUS;
+  options.vel = options.vel || Util.randomVec(DEFAULTS.SPEED);
+
+  MovingObject.call(this, options);
 }
 
+// Must inherit before adding any properties (usually functions) to the Asteroid prototype
 Util.inherits(Asteroid, MovingObject);
 
 Asteroid.prototype.collideWith = function collideWith(otherObject) {
-    if (otherObject instanceof Ship) {
-        otherObject.relocate();
-        return true;
-    } else if (otherObject instanceof Bullet) {
-        this.remove();
-        otherObject.remove();
-        return true;
-    }
-    return false;
+  if (otherObject instanceof Ship) {
+    otherObject.relocate();
+    return true;
+  } else if (otherObject instanceof Bullet) {
+    this.remove();
+    otherObject.remove();
+    return true;
+  }
+  return false;
 };
+
 
 module.exports = Asteroid;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-const Game = require('./game.js');
-const GameView = require('./game_view.js');
+const Game = require("./game");
+const GameView = require("./game_view");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const canvasEl = document.getElementsByTagName("canvas")[0];
+  canvasEl.width = Game.DIM_X;
+  canvasEl.height = Game.DIM_Y;
 
-document.addEventListener("DOMContentLoaded", function (){
-    const canvasEl = document.getElementById("game-canvas");
-    canvasEl.width = Game.DIM_X;
-    canvasEl.height = Game.DIM_Y;
-    const ctx = canvasEl.getContext("2d");
-    
-    const game = new Game();
-    new GameView(game, ctx).start(ctx);
-
-    // const test = new MovingObejct({
-    //     pos: [30, 30],
-    //     vel: [10, 10],
-    //     radius: 5,
-    //     color: "#00FF00"
-    // });
-
-    // const ast = new Asteroid({
-    //     pos: [200,200],
-    //     vel: [1, 1]
-    // })
-    // test.draw(ctx);
-    // ast.draw(ctx);
-    
-})
-
-console.log("webpack is working!");
+  const ctx = canvasEl.getContext("2d");
+  const game = new Game();
+  new GameView(game, ctx).start();
+});
